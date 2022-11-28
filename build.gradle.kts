032888b47e0f63d0b2d9f8db17921364a1cd2f85
= <=>
2022-11-28 18:12:45 +0900

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,51 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "3.0.0"
+	id("io.spring.dependency-management") version "1.1.0"
+	id("org.graalvm.buildtools.native") version "0.9.18"
+	id("org.asciidoctor.convert") version "1.5.8"
+	kotlin("jvm") version "1.7.21"
+	kotlin("plugin.spring") version "1.7.21"
+}
+
+group = "com.sightseekerstudio"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_17
+
+repositories {
+	mavenCentral()
+}
+
+extra["snippetsDir"] = file("build/generated-snippets")
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("io.micrometer:micrometer-tracing-bridge-brave")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.restdocs:spring-restdocs-mockmvc")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "17"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.test {
+	outputs.dir(snippetsDir)
+}
+
+tasks.asciidoctor {
+	inputs.dir(snippetsDir)
+	dependsOn(test)
+}
---
+++ src/main/kotlin/com/sightseekerstudio/demo/springboot3/Springboot3DemoApplication.kt
@@ -0,0 +1,11 @@
+package com.sightseekerstudio.demo.springboot3
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Springboot3DemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<Springboot3DemoApplication>(*args)
+}
